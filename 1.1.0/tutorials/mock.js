e3d4bba6fe7cd0ec2eb76050f1b50edcb61f1664
jein.yi <ahin1127>
2015-05-07 16:12:13 +0900

    1.1.0

---
+++ 1.1.0/tutorials/mock.js
@@ -0,0 +1,58 @@
+dataCallback({
+        "collections" : [
+            {
+                "index" : 0,
+                "items" : [
+                    [ "Shrugs & Cardigans", "27", "30" ],
+                    [ "Sweaters",  "27", "30" ],
+                    [ "Sets",  "27", "30" ],
+                    [ "Shirts & Blouse",  "27", "30"  ],
+                    [ "Shorts",  "27", "30"  ],
+                    [ "Shoes",  "27", "30"  ],
+                    [ "Semiformal",  "27", "30"  ],
+                    [ "Seamless",  "27", "30"  ],
+                    [ "Special Occasion",  "27", "30"  ],
+                    [ "Socks\\/Footwear",  "27", "30"  ]
+                ],
+                "title" : "Category",
+                "type" : "department"
+            },
+            {
+                "index" : 1,
+                "items" : [
+                    [ "Solid Dress", "Casual", "c3,44" ],
+                    [ "Stretch Bracelet", "Bracelets", "c3=153" ],
+                    [ "Striped Top", "Fashion Tops", "c3=254" ],
+                    [ "stripe  cardigan", "Shrugs & Cardigans", "c3=27" ],
+                    [ "SOLID TOP", "Dressy Tops", "c3=18" ],
+                    [ "Sweater top", "Fashion Tops", "c3=254" ],
+                    [ "SOLID  TOP", "Casual", "c3=210" ],
+                    [ "SLEEVELESS TOP", "Fashion Tops", "c3=254" ],
+                    [ "SOLID TOP", "Fashion Tops", "c3=254" ],
+                    [ "STRIPED TOP", "Casual", "c3=210" ]
+                ],
+                "title" : "Product in Category",
+                "type" : "srch_in_department"
+            },
+            {
+                "index" : 2,
+                "items" : [
+                    [ "Skinny jeans" ],
+                    [ "shirt dress" ],
+                    [ "SWEATSHIRT" ],
+                    [ "striped dress" ],
+                    [ "sweat pants" ],
+                    [ "stripe CARDIGAN" ],
+                    [ "SunGlasses" ],
+                    [ "short sleeve" ],
+                    [ "Shopwtd" ],
+                    [ "shorts" ]
+                ],
+                "title" : "Product",
+                "type" : "srch"
+            }
+        ],
+        "query" : [ "s" ],
+        "ver" : "1.0"
+    }
+)
