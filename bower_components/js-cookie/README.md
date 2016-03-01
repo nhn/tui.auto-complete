4becb0a5e7f1ce5b48a29b99691f08600cee0bcf
minkyu-yi <minkyu.yi@nhnent.com>
2016-03-01 17:29:40 +0900

    v1.1.2

---
+++ bower_components/js-cookie/README.md
@@ -0,0 +1,72 @@
+# jquery.cookie
+
+A simple, lightweight jQuery plugin for reading, writing and deleting cookies.
+
+## Installation
+
+Include script *after* the jQuery library (unless you are packaging scripts somehow else):
+
+    <script src="/path/to/jquery.cookie.js"></script>
+
+## Usage
+
+Create session cookie:
+
+    $.cookie('the_cookie', 'the_value');
+
+Create expiring cookie, 7 days from then:
+
+    $.cookie('the_cookie', 'the_value', { expires: 7 });
+
+Create expiring cookie, valid across entire site:
+
+    $.cookie('the_cookie', 'the_value', { expires: 7, path: '/' });
+
+Read cookie:
+
+    $.cookie('the_cookie'); // => "the_value"
+    $.cookie('the_cookie', { raw: true }); // => "the_value" not URL decoded
+    $.cookie('not_existing'); // => null
+
+Delete cookie:
+
+    // returns false => No cookie found
+    // returns true  => A cookie was found
+    $.removeCookie('the_cookie'[, options]);
+
+*Note: when deleting a cookie, you must pass the exact same path, domain and secure options that were used to set the cookie, unless you're relying on the default options that is.*
+
+## Options
+
+Options can be set globally by setting properties of the `$.cookie.defaults` object or individually for each call to `$.cookie()` by passing a plain object to the options argument. Per-call options override the ones set by `$.cookie.defaults`.
+
+    expires: 365
+
+Define lifetime of the cookie. Value can be a `Number` which will be interpreted as days from time of creation or a `Date` object. If omitted, the cookie becomes a session cookie.
+
+    path: '/'
+
+Define the path where the cookie is valid. *By default the path of the cookie is the path of the page where the cookie was created (standard browser behavior).* If you want to make it available for instance across the entire domain use `path: '/'`. Default: path of page where the cookie was created.
+
+    domain: 'example.com'
+
+Define the domain where the cookie is valid. Default: domain of page where the cookie was created.
+
+    secure: true
+
+If true, the cookie transmission requires a secure protocol (https). Default: `false`.
+
+    raw: true
+
+By default the cookie value is encoded/decoded when creating/reading, using `encodeURIComponent`/`decodeURIComponent`. Turn off by setting `raw: true`. Default: `false`.
+
+## Development
+
+- Source hosted at [GitHub](https://github.com/carhartl/jquery-cookie)
+- Report issues, questions, feature requests on [GitHub Issues](https://github.com/carhartl/jquery-cookie/issues)
+
+Pull requests are very welcome! Make sure your patches are well tested. Please create a topic branch for every separate change you make.
+
+## Authors
+
+[Klaus Hartl](https://github.com/carhartl)
---
+++ bower_components/js-cookie/test.js
@@ -0,0 +1,160 @@
+var before = {
+	setup: function () {
+		cookies = document.cookie.split('; ')
+		for (var i = 0, c; (c = (cookies)[i]) && (c = c.split('=')[0]); i++) {
+			document.cookie = c + '=; expires=' + new Date(0).toUTCString();
+		}
+
+		$.cookie.defaults = {};
+	}
+};
+
+
+module('read', before);
+
+test('simple value', 1, function () {
+	document.cookie = 'c=v';
+	equal($.cookie('c'), 'v', 'should return value');
+});
+
+test('empty value', 1, function () {
+	// IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, which
+	// resulted in a bug while reading such a cookie.
+	$.cookie('c', '');
+	equal($.cookie('c'), '', 'should return value');
+});
+
+test('not existing', 1, function () {
+	equal($.cookie('whatever'), null, 'should return null');
+});
+
+test('decode', 1, function () {
+	document.cookie = encodeURIComponent(' c') + '=' + encodeURIComponent(' v');
+	equal($.cookie(' c'), ' v', 'should decode key and value');
+});
+
+test('decode pluses to space for server side written cookie', 1, function () {
+	document.cookie = 'c=foo+bar'
+	equal($.cookie('c'), 'foo bar', 'should convert pluses back to space');
+});
+
+test('raw: true', 1, function () {
+	document.cookie = 'c=%20v';
+	equal($.cookie('c', { raw: true }), '%20v', 'should not decode value');
+});
+
+test('[] used in name', 1, function () {
+	document.cookie = 'c[999]=foo';
+	equal($.cookie('c[999]'), 'foo', 'should return value');
+});
+
+test('embedded equals', 1, function () {
+	$.cookie('c', 'foo=bar', { raw: true });
+	equal($.cookie('c', { raw: true }), 'foo=bar', 'should include the entire value');
+});
+
+test('defaults', 1, function () {
+	document.cookie = 'c=%20v';
+	$.cookie.defaults.raw = true;
+	equal($.cookie('c'), '%20v', 'should use raw from defaults');
+});
+
+
+module('write', before);
+
+test('String primitive', 1, function () {
+	$.cookie('c', 'v');
+	equal($.cookie('c'), 'v', 'should write value');
+});
+
+test('String object', 1, function () {
+	$.cookie('c', new String('v'));
+	equal($.cookie('c'), 'v', 'should write value');
+});
+
+test('value "[object Object]"', 1, function () {
+	$.cookie('c', '[object Object]');
+	equal($.cookie('c'), '[object Object]', 'should write value');
+});
+
+test('number', 1, function () {
+	$.cookie('c', 1234);
+	equal($.cookie('c'), '1234', 'should write value');
+});
+
+test('expires option as days from now', 1, function() {
+	var sevenDaysFromNow = new Date();
+	sevenDaysFromNow.setDate(sevenDaysFromNow.getDate() + 7);
+	equal($.cookie('c', 'v', { expires: 7 }), 'c=v; expires=' + sevenDaysFromNow.toUTCString(),
+		'should write the cookie string with expires');
+});
+
+test('expires option as Date instance', 1, function() {
+	var sevenDaysFromNow = new Date();
+	sevenDaysFromNow.setDate(sevenDaysFromNow.getDate() + 7);
+	equal($.cookie('c', 'v', { expires: sevenDaysFromNow }), 'c=v; expires=' + sevenDaysFromNow.toUTCString(),
+		'should write the cookie string with expires');
+});
+
+test('invalid expires option (in the past)', 1, function() {
+	var yesterday = new Date();
+	yesterday.setDate(yesterday.getDate() - 1);
+	$.cookie('c', 'v', { expires: yesterday });
+	equal($.cookie('c'), null, 'should not save already expired cookie');
+});
+
+test('return value', 1, function () {
+	equal($.cookie('c', 'v'), 'c=v', 'should return written cookie string');
+});
+
+test('raw option set to true', 1, function () {
+	equal($.cookie('c', ' v', { raw: true }).split('=')[1],
+		' v', 'should not encode');
+});
+
+test('defaults', 1, function () {
+	$.cookie.defaults.raw = true;
+	equal($.cookie('c', ' v').split('=')[1], ' v', 'should use raw from defaults');
+});
+
+
+module('delete', before);
+
+test('delete (deprecated)', 1, function () {
+	document.cookie = 'c=v';
+	$.cookie('c', null);
+	equal(document.cookie, '', 'should delete the cookie');
+});
+
+
+module('removeCookie', before);
+
+test('delete', 1, function() {
+	document.cookie = 'c=v';
+	$.removeCookie('c');
+	equal(document.cookie, '', 'should delete the cookie');
+});
+
+test('return', 2, function() {
+	equal($.removeCookie('c'), false, "should return false if a cookie wasn't found");
+
+	document.cookie = 'c=v';
+	equal($.removeCookie('c'), true, 'should return true if the cookie was found');
+});
+
+test('passing options', 2, function() {
+	var oldCookie = $.cookie;
+
+	$.cookie = function(arg0, arg1, arg2) {
+		if (arg1 === null) {
+			equal(arg2.test, 'options', 'The options should be passed');
+		} else {
+			equal(arg1.test, 'options', 'The options should be passed');
+		}
+	};
+
+	document.cookie = 'c=v';
+	$.removeCookie('c', { test: 'options' });
+
+	$.cookie = oldCookie;
+});
